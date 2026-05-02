c00bb11a5da925b117b9935201eae72da868261c
alm0r <almor.dev@protonmail.com>
2026-05-02 17:48:26 +0200

    update the html and css, added to buttons

--- js/main.js
+++ js/main.js
@@ -16,20 +16,13 @@ let board = ["cell-0", "cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6
 //Introduced to keep track of the turn but alsoto switch between players, I think it will also be helpful for declaring a stalemate since I am pretty sure after 9 turns if no one has won the game its supposed to start over.
 let turn = 0;
 //This is just to list the players and their symbols, this will be used to feed to the player object as arguments.
-let players = [["player1", "X"], ["player2", "O"]];
-
-
-
-// This let playerWins = false; for now works as a global variable to determine the outcome of thw switch statement inside checkWinCon()
-// but is unnecesary (and kind of reduntand) and could be replaced byt simply using a return true inside the switch
-// I think its use depends on which kind logic we end up using for the switching between players, but its is posible to make checkWinCon() to simply return
-// a boolean (as i tested in the first case with return true) and then simply check if player1.checWinCon() is returned true or false instead of checking the playerWinds variable
-// i will leave for now because its the first iteration and i think is easy and simpler to understand
-
-let playerWins = false;
+let players = [
+  ["player1", "X"],
+  ["player2", "O"],
+];
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
-// Cells are object because, well, OOP, but still very green
+// Players are cllases following OOP design
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 class Player {
@@ -88,22 +81,12 @@ class Player {
   }
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// This right now does nothing usefull to the actual project. It takes all the ids cells from the html and save them in an array but thats all
-// I was thinking maybe i could use them later on for checking the winning condition logic
-///////////////////////////////////////////////////////////////////////////////////////////
-
-const gameCellsIds = [];
-
-document.querySelectorAll(".game-cell").forEach((element) => {
-  gameCellsIds.push(element.id);
-});
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Main code logic for now, kinda works but is not finnal at all, only on the frontend,
-// Allows you to click on of the cells divs from the html  updates the DOM and creates a cell object but thats abaout it, there is no backend game logic that save that state and/or make use of it
+// Allows you to click on of the cells divs from the html  updates the DOM and creates a cell object but thats abaout it,
+// there is no backend game logic that save that state and/or make use of it
 // Still lacks all the logic (winning condition, game loop, switch between players,etc) Not sure how should i tackle that for now
-////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 document.querySelectorAll(".game-cell").forEach((element) => {
   element.addEventListener("click", () => {
@@ -112,7 +95,10 @@ document.querySelectorAll(".game-cell").forEach((element) => {
     // gets the id number
     let idNumber = element.id[element.id.length - 1];
     // creates player Object and picks the cell after clicking on it on the DOM. This right now is hardcoded because there is no switching between players states logic
-    const player1 = new Player(players[turn % players.length][0], players[turn % players.length][1]); /*Replaced player1 and x for array of players created earlier, turn will change and shift the array and wrap around once it exceeds its length*/
+    const player1 = new Player(
+      players[turn % players.length][0],
+      players[turn % players.length][1],
+    ); /*Replaced player1 and x for array of players created earlier, turn will change and shift the array and wrap around once it exceeds its length*/
     turn += 1; /*to shift player array*/
     player1.createCell(true, idNumber);
     player1.checkWinCon();
@@ -126,12 +112,57 @@ document.querySelectorAll(".game-cell").forEach((element) => {
 
 // console.log(gameCellsIds);
 
-// * the paramter player-1 inside the choosenCell is hardcoded for testing purpose but it should be dynacmic. That means refactoring and using a variable, turning the anon arrow function into a proper one and passing a paramater,
-// using a methord, switching with a loop...plenty of Options.
+//////////////////////////////////////////////////////////////////////////////////////////
+// This right now does nothing usefull to the actual project.
+// It takes all the ids cells from the html and save them in an array but thats all
+// I was thinking maybe i could use them later on for checking the winning condition logic
+///////////////////////////////////////////////////////////////////////////////////////////
+
+const gameCellsIds = [];
+
+document.querySelectorAll(".game-cell").forEach((element) => {
+  gameCellsIds.push(element.id);
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// FOR FUTURE REFACTORING DO NOT DELETE
 
 ///////////////////////////////////////////////////
 // SWITCHING BETWEEN PLAYERS LOGIC PROTOTYPE
 /////////////////////////////////////////////////
+
+// This let playerWins = false; for now works as a global variable to determine the outcome of thw switch statement inside checkWinCon()
+// but is unnecesary (and kind of reduntand) and could be replaced byt simply using a return true inside the switch
+// I think its use depends on which kind logic we end up using for the switching between players, but its is posible to make checkWinCon() to simply return
+// a boolean (as i tested in the first case with return true) and then simply check if player1.checWinCon() is returned true or false instead of checking the playerWinds variable
+// i will leave for now because its the first iteration and i think is easy and simpler to understand
+
+let playerWins = false;
 
 // This works by using the global variable let gameBeingPlayed = true; in a while loop. The while loop switches between 2 states determined byt 2 other global variables
 // let playerOneTurn = true; and // let playerTwoTurn = false; and alternates between them. While one of the if statements is true, it runs, and at then end, it becames false and changes
